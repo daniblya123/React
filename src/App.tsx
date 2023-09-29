d9d2e98c32ef63c27fde98d1f091efcc75db41dc
Alex Bowker <alex.bowker-lonnecker@salfordcc.ac.uk>
2023-09-29 11:41:36 +0100

    Change HOME_PATH constant to INDEX_PATH

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import NewsPage from "./pages/NewsPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import { HOME_PATH, NEWS_PATH, NOT_FOUND_PATH } from "./constants/paths";
+import { INDEX_PATH, NEWS_PATH, NOT_FOUND_PATH } from "./constants/paths";
 
 export function App() {
   return (
     <>
       <Routes>
-        <Route path={HOME_PATH} element={<HomePage />} />
+        <Route path={INDEX_PATH} element={<HomePage />} />
         <Route path={NEWS_PATH} element={<NewsPage />} />
         <Route path={NOT_FOUND_PATH} element={<NotFoundPage />} />
       </Routes>
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { HOME_PATH } from "../constants/paths";
+import { INDEX_PATH } from "../constants/paths";
 
 function NotFoundPage() {
   useEffect(() => {
@@ -11,7 +11,7 @@ function NotFoundPage() {
     <>
       <h1>This is the 404 Page.</h1>
       <p>
-        Here is a link back to the <Link to={HOME_PATH}>Home Page.</Link>
+        Here is a link back to the <Link to={INDEX_PATH}>Home Page.</Link>
       </p>
     </>
   );
